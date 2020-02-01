96728cea941f38bb4f9684748742782cf4927ab4
Bruno edwards <bruno.edwards@stint.co>
2020-02-01 14:30:24 +0000

    added image scaling

--- main.py
+++ main.py
@@ -12,6 +12,35 @@ display_surface=pygame.display.set_mode((0,0), FULLSCREEN)
 KEEP="like/"
 DELETE="dislike/"
 
+#modified from http://www.pygame.org/pcr/transform_scale/
+def aspect_scale(img,bx,by):
+    """ Scales 'img' to fit into box bx/by.
+     This method will retain the original image's aspect ratio """
+    ix,iy = img.get_size()
+    if ix > iy:
+        # fit to width
+        scale_factor = bx/float(ix)
+        sy = scale_factor * iy
+        if sy > by:
+            scale_factor = by/float(iy)
+            sx = scale_factor * ix
+            sy = by
+        else:
+            sx = bx
+    else:
+        # fit to height
+        scale_factor = by/float(iy)
+        sx = scale_factor * ix
+        if sx > bx:
+            scale_factor = bx/float(ix)
+            sx = bx
+            sy = scale_factor * iy
+        else:
+            sy = by
+
+    return pygame.transform.scale(img, (int(sx), int(sy)))
+
+
 def display_image():
     #the current image path
     global image_path
@@ -25,8 +54,11 @@ def display_image():
     #load it
     image=pygame.image.load(image_path)
 
+    #resize image but keep aspect ratio
+    scaled_image=aspect_scale(image, 1920, 1080)
+
     #display the image
-    display_surface.blit(image, (0,0))
+    display_surface.blit(scaled_image, (0,0))
 
 #called when up gets pressed
 def up_pressed():
@@ -64,5 +96,6 @@ while 1:
             #call the appropriate function
             if event.key==K_UP:up_pressed()
             if event.key==K_DOWN:down_pressed()
+
     #update the display
     pygame.display.update()
